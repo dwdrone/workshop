331198bece4b8860a14cfd44b88011d16180512a
dwdrone <darkwolf.drones@gmail.com>
2026-04-20 08:55:46 -0600

    openwrt setup

---
+++ setup/openwrt/download-openwrt.sh
@@ -0,0 +1,19 @@
+
+
+echo Please edit this script to download the images you need (tbd).
+
+# OpenWrt
+# wget https://downloads.openwrt.org/releases/23.05.0/targets/ath79/nand/openwrt-23.05.0-ath79-nand-glinet_gl-ar300m-nand-initramfs-kernel.bin
+# wget https://downloads.openwrt.org/releases/23.05.0/targets/ath79/nand/openwrt-23.05.0-ath79-nand-glinet_gl-ar300m-nand-squashfs-factory.img
+# wget https://downloads.openwrt.org/releases/23.05.0/targets/ath79/nand/openwrt-23.05.0-ath79-nand-glinet_gl-ar300m-nand-squashfs-sysupgrade.bin
+# wget https://downloads.openwrt.org/releases/25.12.2/targets/ath79/nand/openwrt-25.12.2-ath79-nand-glinet_gl-ar300m-nand-initramfs-kernel.bin
+# wget https://downloads.openwrt.org/releases/25.12.2/targets/ath79/nand/openwrt-25.12.2-ath79-nand-glinet_gl-ar300m-nand-squashfs-factory.img
+# wget https://downloads.openwrt.org/releases/25.12.2/targets/ath79/nand/openwrt-25.12.2-ath79-nand-glinet_gl-ar300m-nand-squashfs-sysupgrade.bin
+# wget https://downloads.openwrt.org/releases/25.12.2/targets/ath79/nand/openwrt-25.12.2-ath79-nand-glinet_gl-ar300m-nor-initramfs-kernel.bin
+# wget https://downloads.openwrt.org/releases/25.12.2/targets/ath79/nand/openwrt-25.12.2-ath79-nand-glinet_gl-ar300m-nor-squashfs-sysupgrade.bin
+
+# https://dl.gl-inet.com/router/ar300m16/stable
+# wget https://fw.gl-inet.com/firmware/ar300m/release4/openwrt-ar300m16-4.3.7-0913-1694589994.bin
+
+# https://github.com/gl-inet/uboot-for-qca95xx/
+# https://github.com/gl-inet/uboot-for-qca95xx/blob/master/bin/uboot-gl-ar300m-20220216-md5-97ff7bb80cbf129fa21f34ded1559ff8.bin
